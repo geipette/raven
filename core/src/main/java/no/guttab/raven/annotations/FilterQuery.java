29b7bd4c0704e6001c870e9877f7104361027754
Geir Henning Pettersen <geir@guttab.no>
2012-02-14 22:42:48 +0100

    Added documentation for filterQuery annotation to demonstrate how it can be documented.

--- core/src/main/java/no/guttab/raven/annotations/FilterQuery.java
+++ core/src/main/java/no/guttab/raven/annotations/FilterQuery.java
@@ -10,6 +10,22 @@ import no.guttab.raven.search.query.StringValueFilterQueryCriteriaBuilder;
 
 import static no.guttab.raven.annotations.CombineOperator.AND;
 
+/**
+ * A field annotated with this annotation can contain one or more filterValues that should
+ * be filtered on.
+ * <p/>
+ * Examples:
+ * <p/>
+ * <hr/>
+ * Boolean filter that only include documents with inStock = <tt>true</tt>:
+ * <pre>
+ * class SearchRequest {
+ *    &#64;FilterQuery
+ *    Boolean inStock = true;
+ * }
+ * </pre>
+ * <hr/>
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.FIELD})
 @Documented
