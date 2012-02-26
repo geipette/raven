f05394d36a8fb4bc709a09a5e01c9fbb64c5749b
Geir Henning Pettersen <geir@guttab.no>
2012-02-26 23:59:02 +0100

    WIP

--- core/src/main/java/no/guttab/raven/search/query/SortQueryProcessor.java
+++ core/src/main/java/no/guttab/raven/search/query/SortQueryProcessor.java
@@ -1,22 +1,28 @@
 package no.guttab.raven.search.query;
 
-import java.lang.reflect.Field;
-
 import no.guttab.raven.annotations.AnnotatedFieldCallback;
 import no.guttab.raven.annotations.AnnotationUtils;
 import no.guttab.raven.annotations.Sort;
 import org.apache.solr.client.solrj.SolrQuery;
 
+import java.lang.reflect.Field;
+
 import static no.guttab.raven.reflection.FieldUtils.getFieldValue;
+import static org.apache.commons.lang3.StringUtils.isEmpty;
 
 public class SortQueryProcessor implements QueryProcessor {
-   @Override
-   public void buildQuery(final Object queryInput, final SolrQuery solrQuery) {
-      AnnotationUtils.doForFirstAnnotatedFieldOn(queryInput, Sort.class, new AnnotatedFieldCallback<Sort>() {
-         @Override
-         public void doFor(Field field, Sort annotation) {
-            solrQuery.setSortField((String) getFieldValue(field, queryInput), SolrQuery.ORDER.asc);
-         }
-      });
-   }
+
+
+    @Override
+    public void buildQuery(final Object queryInput, final SolrQuery solrQuery) {
+        AnnotationUtils.doForFirstAnnotatedFieldOn(queryInput, Sort.class, new AnnotatedFieldCallback<Sort>() {
+            @Override
+            public void doFor(Field field, Sort annotation) {
+                String value = (String) getFieldValue(field, queryInput);
+                if (!isEmpty(value)) {
+                    solrQuery.setSortField(value, SolrQuery.ORDER.asc);
+                }
+            }
+        });
+    }
 }
--- core/src/test/java/no/guttab/raven/search/query/SortQueryProcessorTest.java
+++ core/src/test/java/no/guttab/raven/search/query/SortQueryProcessorTest.java
@@ -2,6 +2,7 @@ package no.guttab.raven.search.query;
 
 import no.guttab.raven.annotations.Sort;
 import org.apache.solr.client.solrj.SolrQuery;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
@@ -13,25 +14,44 @@ import static org.junit.Assert.fail;
 @RunWith(MockitoJUnitRunner.class)
 public class SortQueryProcessorTest {
 
-   @Mock
-   private SolrQuery solrQuery;
+    @Mock
+    private SolrQuery solrQuery;
 
-   @Test
-   public void when_sort_annotation_defined_and_it_contains_a_value_sort_should_be_set_on_query() throws Exception {
-      class TestQuery {
-         @Sort
-         String sortField = "popularity";
-      }
+    @Test
+    public void when_sort_annotation_defined_and_it_contains_a_value_sort_should_be_set_on_query() throws Exception {
+        class TestQuery {
+            @Sort
+            String sortField = "popularity";
+        }
 
-      SortQueryProcessor queryProcessor = new SortQueryProcessor();
-      queryProcessor.buildQuery(new TestQuery(), solrQuery);
+        SortQueryProcessor queryProcessor = new SortQueryProcessor();
+        TestQuery input = new TestQuery();
+        queryProcessor.buildQuery(input, solrQuery);
 
-      Mockito.verify(solrQuery).setSortField("popularity", SolrQuery.ORDER.asc);
-   }
+        Mockito.verify(solrQuery).setSortField("popularity", SolrQuery.ORDER.asc);
+    }
 
-   @Test
-   public void fail_this_test() throws Exception {
-      // TODO: Implement @SortOrder, @SortTarget and @SortVariant
-      fail("Not implemented");
-   }
+    @Test
+    public void when_sort_annotation_defined_and_it_is_empty_no_sort_field_should_be_set() throws Exception {
+        class TestQuery {
+            @Sort
+            String sortField;
+        }
+
+        SortQueryProcessor queryProcessor = new SortQueryProcessor();
+        TestQuery input = new TestQuery();
+        queryProcessor.buildQuery(input, solrQuery);
+
+        Mockito.verifyNoMoreInteractions(solrQuery);
+    }
+
+
+    @Test
+    @Ignore
+    public void fail_this_test() throws Exception {
+        // TODO: Implement @SortOrder, @SortTarget and @SortVariant
+        fail("Not implemented");
+    }
+
+
 }
