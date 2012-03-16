cfc5b66f6d4dd6f3cf37fd36265d53db3e48a767
Geir Henning Pettersen <geir@guttab.no>
2012-03-16 23:19:16 +0100

    #17 sorting navigation wip

--- core/src/main/java/no/guttab/raven/search/response/navigators/page/PageNavigation.java
+++ core/src/main/java/no/guttab/raven/search/response/navigators/page/PageNavigation.java
@@ -4,7 +4,7 @@ import org.apache.solr.client.solrj.response.QueryResponse;
 
 import static org.apache.commons.lang3.StringUtils.isEmpty;
 
-public class PageNavigation {
+class PageNavigation {
     private static final int FIRST_PAGE = 1;
     private PageConfig pageConfig;
     private QueryResponse queryResponse;
@@ -26,16 +26,16 @@ public class PageNavigation {
         if (pageConfig.getResultsPerPage() == 0) {
             return FIRST_PAGE;
         } else {
-            return calculatePage();
+            return calculateCurrentPage();
         }
+    }
+
+    public boolean resultsStartsAtZero() {
+        return getCurrentResultStartIndex() == 0;
     }
 
     public long getCurrentResultStartIndex() {
         return queryResponse.getResults().getStart();
-    }
-
-    public boolean resultsStartsAtZero() {
-        return queryResponse.getResults().getStart() == 0;
     }
 
     public String getPageRequestFieldName() {
@@ -47,10 +47,10 @@ public class PageNavigation {
     }
 
     public boolean requestHasStartIndexFieldName() {
-        return !isEmpty(pageConfig.getStartRequestFieldName());
+        return !isEmpty(getStartRequestFieldName());
     }
 
-    private long calculatePage() {
+    private long calculateCurrentPage() {
         return queryResponse.getResults().getStart() / pageConfig.getResultsPerPage() + FIRST_PAGE;
     }
 }
